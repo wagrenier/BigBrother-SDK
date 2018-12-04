141c5efa84d20cbefb1dcf747a9d866ae11d2b4b
lilopido <chao-shadow@hotmail.com>
2018-12-03 22:33:11 -0500

    groupListingChange

--- src/main/java/Dto/GroupListingInfo.java
+++ src/main/java/Dto/GroupListingInfo.java
@@ -7,17 +7,18 @@ public class GroupListingInfo {
   public int max_par_groupe = 0;
   public String prefixe = "";
   public int groupId;
-
+  public String created_by = "";
   protected GroupListingInfo(){
 
   }
 
-  public GroupListingInfo(String activite, String trimestre_id, int nombreGroupe, int max_par_groupe, int groupId, String prefixe) {
+  public GroupListingInfo(String activite, String trimestre_id, int nombreGroupe, int max_par_groupe, int groupId, String prefixe, String created_by) {
     this.activite = activite;
     this.trimestre_id = trimestre_id;
     this.nombreGroupe = nombreGroupe;
     this.max_par_groupe = max_par_groupe;
     this.groupId = groupId;
-    this.prefixe = prefixe;
+	this.prefixe = prefixe;
+	this.created_by = created_by;
   }
 }
